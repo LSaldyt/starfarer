cd5ead3dd04b48b053790a27dc9c80eb002541a3
LSaldyt <lucassaldyt@gmail.com>
2019-06-30 09:49:52 -0700

    Adds control.py and bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+from control import write, press, combo, click
+import subprocess
+
+class Bot:
+    def __init__(self):
+        pass
+
+def main(args):
+    combo('ctrl', '`')
+    txt = 'echo "This text was written by a bot!"'
+    write(txt)
+    press('enter')
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
