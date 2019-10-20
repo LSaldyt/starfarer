f3fab0497f04d83864a9d981125edc79f9ae1f5e
LSaldyt <lucassaldyt@gmail.com>
2019-10-20 14:05:52 -0700

    Adds rudimentary starsector bot1000, 500

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 from control import write, press, combo, click
+from time import sleep
+import pyautogui as p
 import subprocess, sys
 
 class Bot:
@@ -6,10 +8,23 @@ class Bot:
         pass
 
 def main(args):
-    combo('ctrl', '`')
-    txt = 'echo "This text was written by a bot!"'
-    write(txt)
-    press('enter')
+    combo('alt', '4') # Switch to game screen
+    press('space')
+    x, y = p.size()
+    cx, cy = x // 2, y // 2
+    sleep(0.1)
+    click(cx, cy, button='right') # Raise shields
+    p.keyDown('w')
+    while True:
+        click(cx, cy - 50, clicks=10)
+        sleep(0.1)
+    p.keyUp('w')
+    press('space')
+
+    #combo('ctrl', '`')
+    #txt = 'echo "This text was written by a bot!"'
+    #write(txt)
+    #press('enter')
     return 0
 
 if __name__ == '__main__':
